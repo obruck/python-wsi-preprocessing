9450974b704ec519211a7bd01da7cf0641758774
obruck <oscar.bruck@helsinki.fi>
2019-03-05 08:27:45 +0200

    setup.py

---
+++ deephistopath/wsi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(name='python-wsi-preprocessing',
+      version='1.0',
+      url="https://github.com/obruck/python-wsi-preprocessing",
+      author="deroneriksson",
+      packages = find_packages()
+)
